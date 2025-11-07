a28f5041c89dc853f0f76ba60e11de860e8c77d3
razerx100 <saikatmandal8@gmail.com>
2025-11-07 23:46:44 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Instructions
+
+* Install NeoVim.
+
+* Optionally install a Nerd Font
+
+* Copy the `nvim` folder to `~/.config/` on UNIX systems. `~\AppData\Local\` on Windows.
+
---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,26 @@
+local lazyPath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazyPath) then
+    local lazyRepo = "https://github.com/folke/lazy.nvim.git"
+    local out      = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyRepo, lazyPath })
+
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+	    { "\nPress any key to exit..." }
+        }, true, {})
+
+	vim.fn.getchar()
+	os.exit(1)
+    end
+end
+
+vim.opt.rtp:prepend(lazyPath)
+
+vim.g.mapleader      = " "
+vim.g.maplocalleader = "\\"
+
+require("lazy").setup({
+    -- spec    = { import = "plugins" },
+    -- checker = { enable = true }
+})
