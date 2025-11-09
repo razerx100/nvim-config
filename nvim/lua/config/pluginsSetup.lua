fe69d4d8472b5458ed36127ab48c8affea3a8dea
razerx100 <saikatmandal8@gmail.com>
2025-11-09 23:13:13 +0000

    Add auto-complete mapping

--- nvim/lua/config/pluginsSetup.lua
+++ nvim/lua/config/pluginsSetup.lua
@@ -25,3 +25,30 @@ do
 	vim.keymap.set("n", "<space>ft", tsBuiltin.live_grep)
 end
 
+-- Auto-complete
+do
+	local cmp = require("cmp")
+
+	cmp.setup({
+		mapping = {
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					local entry = cmp.get_selected_entry()
+
+			       if not entry then
+			         cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+			       end
+
+				    cmp.confirm()
+			    else
+			       fallback()
+			     end
+			end, {"i","s",}),
+			["<C-w>"]     = cmp.mapping.select_prev_item(),
+			["<C-s>"]     = cmp.mapping.select_next_item(),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<C-e>"]     = cmp.mapping.abort(),
+		}
+	})
+end
+
