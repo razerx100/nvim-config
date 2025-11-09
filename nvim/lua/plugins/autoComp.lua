3cdd18983239d09f584ed7b8c677bcbe68354764
razerx100 <saikatmandal8@gmail.com>
2025-11-09 20:11:36 +0000

    Add completion for buffers

--- nvim/lua/plugins/autoComp.lua
+++ nvim/lua/plugins/autoComp.lua
@@ -1,10 +1,26 @@
 return {
 	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
 	{
 		"hrsh7th/nvim-cmp",
 		opts = {
 			sources = {
-				{ name = 'nvim_lsp' }
+				{ name = "nvim_lsp" },
+				{
+					name   = "buffer",
+					option = {
+						get_bufnrs = function()
+							local buf       = vim.api.nvim_get_current_buf()
+							local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+
+							if byte_size > 1024 * 1024 then -- 1 Megabyte max
+							   return {}
+							end
+
+							return { buf }
+						end
+					}
+				}
 			}
 		}
 	}
