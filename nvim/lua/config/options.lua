f020cc548802dc6a608a4dc39c8822e11cd7a62a
razerx100 <saikatmandal8@gmail.com>
2025-11-13 23:32:35 +0000

    Turn autoread on

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,17 +1,18 @@
 vim.wo.number = true
 
-vim.opt.encoding      = "utf-8"
-vim.opt.scrolloff     = 7 -- Scroll offset
-vim.opt.tabstop       = 4 -- Tab visibility in spaces
-vim.opt.softtabstop   = 4 -- How many spaces when tab is pressed
-vim.opt.shiftwidth    = 4 -- Indent in spaces
 vim.opt.autoindent    = true
-vim.opt.termguicolors = true
-vim.opt.wrap          = false
+vim.opt.autoread      = true
 vim.opt.clipboard     = "unnamedplus"
 vim.opt.confirm       = true
-vim.opt.expandtab     = true
 vim.opt.cursorline    = true
+vim.opt.encoding      = "utf-8"
+vim.opt.expandtab     = true
+vim.opt.scrolloff     = 7 -- Scroll offset
+vim.opt.shiftwidth    = 4 -- Indent in spaces
+vim.opt.softtabstop   = 4 -- How many spaces when tab is pressed
+vim.opt.tabstop       = 4 -- Tab visibility in spaces
+vim.opt.termguicolors = true
+vim.opt.wrap          = false
 
 vim.cmd[[colorscheme tokyonight]]
 
