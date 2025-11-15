4c1306c6225eea864e34df6d61739259ee9f2add
razerx100 <saikatmandal8@gmail.com>
2025-11-15 14:56:09 +0000

    Add linter

---
+++ nvim/lua/plugins/linter.lua
@@ -0,0 +1,42 @@
+return {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    opts  = {
+        events        = { "BufReadPost", "BufWritePost", "InsertLeave" },
+        linters_by_ft = {
+--            cpp = { "clangtidy" } -- clangd already has clangtidy embedded.
+        }
+    },
+    config = function(_, opts)
+        local lint = require("lint")
+
+        lint.linters_by_ft = opts.linters_by_ft
+
+        function Delay(ms, fn)
+            local timer = vim.uv.new_timer()
+
+            return function(...)
+                if timer then
+                    local argv = { ... }
+
+                    timer:start(ms, 0, function()
+                        timer:stop()
+                        vim.schedule_wrap(fn)(unpack(argv))
+                    end)
+                end
+            end
+        end
+
+        function Lint()
+            local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+
+            if #names > 0 then
+                lint.try_lint(names)
+            end
+        end
+
+        vim.api.nvim_create_autocmd(opts.events, {
+            callback = Delay(100, Lint)
+        })
+    end
+}
