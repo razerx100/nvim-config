cdc69e59ec3d1f2750f6f16b5a4cae896ca2680e
razerx100 <saikatmandal8@gmail.com>
2025-11-22 14:53:11 +0000

    Add arg expansion

--- nvim/lua/config/dapConfig.lua
+++ nvim/lua/config/dapConfig.lua
@@ -1,9 +1,34 @@
+function ArgSplitter(arg)
+	local arg_list = {}
+
+	for str in string.gmatch(arg, "%S+") do
+		table.insert(arg_list, str)
+	end
+
+	return arg_list
+end
+
+function ParseArgs(args)
+	if type(args) == "string" then -- For now assume, we only need to parse if there is one arg. Which should be a string.
+		return ArgSplitter(args)
+	end
+
+	return args
+end
+
 local dap = require("dap")
 
 dap.adapters.cppdbg = {
 	id = "cppdbg",
 	type = "executable",
 	command = vim.fn.stdpath("data") .. "/mason/bin/OpenDebugAD7",
+	enrich_config = function(config, on_config)
+		if config.args then
+			config.args = ParseArgs(config.args)
+		end
+
+		on_config(config)
+	end,
 }
 
 dap.adapters.python = function(cb, config)
@@ -17,6 +42,13 @@ dap.adapters.python = function(cb, config)
 			options = {
 				source_filetype = "python",
 			},
+			enrich_config = function(eConfig, on_config)
+				if eConfig.args then
+					eConfig.args = ParseArgs(eConfig.args)
+				end
+
+				on_config(eConfig)
+			end,
 		})
 	else
 		cb({
@@ -26,6 +58,13 @@ dap.adapters.python = function(cb, config)
 			options = {
 				source_filetype = "python",
 			},
+			enrich_config = function(eConfig, on_config)
+				if eConfig.args then
+					eConfig.args = ParseArgs(eConfig.args)
+				end
+
+				on_config(eConfig)
+			end,
 		})
 	end
 end
