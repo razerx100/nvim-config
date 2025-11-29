f287fe33c9e7730a4f2c005d857d3c275f56d14c
razerx100 <saikatmandal8@gmail.com>
2025-11-29 23:49:54 +0000

    Add harpoon

--- nvim/lua/plugins/vimIlluminate.lua
+++ nvim/lua/plugins/vimIlluminate.lua
@@ -1,14 +1,14 @@
 return {
-    "RRethy/vim-illuminate",
+	"RRethy/vim-illuminate",
 	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    opts = {
-        delay = 200,
-        large_file_cutoff = 2000,
-        large_file_overrides = {
-            providers = { "lsp" }
-        }
-    },
-    config = function (_, opts)
-        require("illuminate").configure(opts)
-    end
+	opts = {
+		delay = 200,
+		large_file_cutoff = 2000,
+		large_file_overrides = {
+			providers = { "lsp" },
+		},
+	},
+	config = function(_, opts)
+		require("illuminate").configure(opts)
+	end,
 }
