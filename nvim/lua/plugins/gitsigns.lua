e0de18228bb0e942d7fa92a567a895937d3a15b7
razerx100 <saikatmandal8@gmail.com>
2025-12-11 23:57:27 +0000

    Add keybindings for blame toggle

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -51,6 +51,8 @@ return {
 			map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
 			map("n", "<leader>ghd", gs.diffthis, "Diff This")
 			map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+			map("n", "<leader>gtb", gs.toggle_current_line_blame, "Toggle line blame")
+			map("n", "<leader>gtw", gs.toggle_word_diff, "Toggle word diff")
 			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
 		end
 	}
